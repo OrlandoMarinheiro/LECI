b9a823c0fec69b1ab40d0b6931189c3263c9d557
Orlando Marinheiro <orlandomarinheiro@ua.pt>
2024-09-24 01:56:20 +0100

    Aula02 - update

--- 3_Ano/IA/Aula01-02/aula2.py
+++ 3_Ano/IA/Aula01-02/aula2.py
@@ -7,7 +7,7 @@ impar = lambda x: x % 2 != 0
 positivo = lambda x: x > 0
 
 #Exercicio 4.3
-comparar_modulo = lambda x, y: x < y
+comparar_modulo = lambda x, y: math.fabs(x) < math.fabs(y)
 
 #Exercicio 4.4 #duvida: posso usar biblioteca math
 cart2pol = lambda x,y: (math.sqrt((x**2 + y**2)), math.atan2(y,x))
@@ -21,17 +21,19 @@ def quantificador_universal(lista, f):
 
 #Exercicio 4.8
 def subconjunto(lista1, lista2):
-    pass
+    return all(x in lista2 for x in lista1)
 
-#Exercicio 4.9
+#Exercicio 4.9 duvida
 def menor_ordem(lista, f):
-    pass
+    return min(lista, key=f)
 
-#Exercicio 4.10
+#Exercicio 4.10 duvida
 def menor_e_resto_ordem(lista, f):
-    pass
+    menor = min(lista, key=f)
+    restante = [x for x in lista if x != menor]
+    return menor, restante
 
-#Exercicio 5.2
+#Exercicio 5.2 duvida
 def ordenar_seleccao(lista, ordem):
     pass
 
@@ -42,11 +44,13 @@ def main():
     print(positivo(-1))
     print(positivo(2))
     print("############")
-    print(comparar_modulo(2,5))
+    print(comparar_modulo(-4,2))
     print(comparar_modulo(5,2))
     print("############")
     print(cart2pol(3,4))
     print("############")
+    print(subconjunto([1,2,3,4], [4,3,2,1]))
+    print(subconjunto([1,2,3,4], [4,5,6,1]))
     
 if __name__ == "__main__":
     main()
