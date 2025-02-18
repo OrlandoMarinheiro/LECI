db47ea0a19c65de9c48e5c7bf458be1f085c2415
Orlando Marinheiro <orlandomarinheiro@ua.pt>
2025-02-18 21:17:49 +0000

    ex5

--- 3_Ano/C/Aula01/code/ex4.java
+++ 3_Ano/C/Aula01/code/ex4.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class ex4 {
     
     public static void main(String[] args) {
-
         HashMap<String, Integer> nums = new HashMap<String, Integer>();
 
         String input;
@@ -36,15 +35,16 @@ public class ex4 {
 
 
             String[] nums_args = input.split("[\\s-]+");
+
+            System.out.print("A list of numbers: ");
             for (String num : nums_args) {
-                System.out.print(num + " ");
+                int result = findNumber(num, nums);
+                if (result == 0) {
+                    continue;
+                }
+                System.out.print(findNumber(num, nums) + " ");
             }
-            System.out.print("-> ");
-            int result = findNumber(nums_args, nums);
-            if (result == 0) {
-                return;
-            }
-            System.out.println(findNumber(nums_args, nums));
+            System.out.println();
 
         } catch (Exception e) {
             System.out.println("Erro ao ler o ficheiro");
@@ -52,43 +52,12 @@ public class ex4 {
 
     }
 
-    public static int findNumber(String[] nums_args, HashMap<String, Integer> nums) {
-        int result = 0;
+    public static int findNumber(String num, HashMap<String, Integer> nums) {
         
-        for (int i = 0; i < nums_args.length; i++) {
-            Integer currentNum = nums.get(nums_args[i]);
-
-            if (currentNum == null) {
-                if (nums_args[i].equals("and")) {
-                    continue;
-                } 
-                System.out.println("Número desconhecido: " + nums_args[i]);
-                return 0;
-            }
-            
-            int j = i + 1;
-            int cnt = 1;
-            while (true) {
-                if (j < nums_args.length) { 
-                    Integer nextNum = nums.get(nums_args[j]);
-                    if (nextNum != null && nextNum >= 100){
-                        cnt = cnt * nextNum;
-                        i++;
-                    } else {
-                        break;
-                    }   
-                } else {
-                    break;
-                }
-                j++; 
-            }
-            if (currentNum > 0 && currentNum <= 9 && cnt > 1) {
-                result += currentNum * cnt;
-            } else {
-                result += currentNum;
-            }  
+        Integer currentNum = nums.get(num);
+        if (currentNum != null) {
+            return currentNum;
         }
-        return result;
+        return 0;
     }
-    
 }
